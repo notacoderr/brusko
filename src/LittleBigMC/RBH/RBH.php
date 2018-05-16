f85d99cca38ab0219aa2fbdbe565eb74fa5249f9
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-05-16 14:36:26 +0800

    Update RBH.php

--- src/LittleBigMC/RBH/RBH.php
+++ src/LittleBigMC/RBH/RBH.php
@@ -155,14 +155,14 @@ public function onDamage(EntityDamageEvent $event)
 				$event->setCancelled();
 				return true;
 			}
-			if(in_array($a, $this->isplayingrbh) && in_array($b, $this->isplayingrbh))
+			if(in_array($a, $this->isplayingrbh) && in_array($event->getEntity()->getLevel()->getFolderName(), $this->rbharenas))
 			{
+				$event->setCancelled(false); //for other plugin's cancelling damage event
+				
 				if($event->getCause() == 2)
 				{
 					$event->setDamage(0.0); //hack, to remove damage on projectile hit entity event
 				}
-				
-				$event->setCancelled(false); //for other plugin's cancelling damage event
 				
 				if($event->getDamage() >= $event->getEntity()->getHealth())
 				{
@@ -198,7 +198,7 @@ private function notifyPlayer(Player $player, int $type): void
 		break;
 
 		case 2:
-			$player->addTitle("", "§7c+1 Death");
+			$player->addTitle("", "§c+1 Death");
 		break;
 	}
 }
